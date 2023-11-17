2e6bf294b18316c7cf53c81b64c0c15ac568304f
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-17 19:26:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const makeChange = (c) => {
+const makeChange = (c) => { 
   // your name here
+
+	let obj={ 
+		'q':25,
+		'd':10,
+		'n':5,
+		'p':1
+	}
+
+	let oj={};
+
+	for(let i=0;i<4;i++)
+		{
+			let num=obj[i];
+
+			let value1=c/num;
+			c-=c%num;
+
+			oj.push(obj[i]:value1);
+			
+		}  
+
+	return oj;
 };
 
 // Do not the change the code below
